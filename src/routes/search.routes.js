1aa5121ea98205a6f91a93da5937772f4df5ef1d
basboosa05 <166862890+basboosa05@users.noreply.github.com>
2026-04-20 15:29:38 +0200

    added route and PostgreSQL Trigram

---
+++ src/routes/search.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const searchController = require('../controllers/search.controller');
+const { authenticate } = require('../middleware/auth');
+ 
+// GET /search
+router.get('/search', searchController.search);
+//router.get('/resolve', searchController.resolve);
+ 
+module.exports = router;
+ 
